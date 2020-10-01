c862a59c0e5b2195873c1c5378fd66a69e1f5c33
rut503 <rutvik503@gmail.com>
2020-10-01 01:03:22 -0500

    all done except need to format the billamountperperson and tipamount to proper currency format and documenting the code

--- CSCI321_Assign2/ViewController.swift
+++ CSCI321_Assign2/ViewController.swift
@@ -12,10 +12,43 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var amountTextField: UITextField!
     
-    @IBOutlet weak var tipAmountTextField: UILabel!
-    @IBOutlet weak var tipPercentageTextField: UILabel!
-    @IBOutlet weak var partySizeTextField: UILabel!
-    @IBOutlet weak var outputAmoutShareTextField: UILabel!
+    
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    
+    @IBOutlet weak var partySizeLabel: UILabel!
+    @IBOutlet weak var billAmountPerPersonLabel: UILabel!
+    
+    var billAmount: Double = 0.0 {
+        didSet{
+            updateView()
+        }
+    }
+    
+    var tipPercent: Int = 20 {
+        didSet{
+            updateView()
+        }
+    }
+    
+    var tipAmount: Double = 0.0 {
+        didSet{
+            updateView()
+        }
+    }
+    
+    var partySize: Int = 1 {
+        didSet{
+            updateView()
+        }
+    }
+    
+    var billAmountPerPerson: Double = 0.0 {
+        didSet{
+            updateView()
+        }
+    }
+    
     // declaring object for amountFieldDelegate class
     let delegate = amountFieldDelegate()
     
@@ -25,30 +58,47 @@ class ViewController: UIViewController, UITextFieldDelegate {
         amountTextField.delegate = delegate
     }
     
-    /**
-    * Closes the keyboard when user touches on other parts of the screen
-    * and also converts the amountTextField to floating point and checks if the field is valid or not using
-    *  amoutFieldDelegate class
-    */
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    @IBAction func enterBillAmountButtonPress(_ sender: UIButton) {
+        
+        // hide the keyboard
         self.view.endEditing(true)
         
+        // get the input as string from the text field
         guard let amount = amountTextField.text, !amount.isEmpty else {
-            alert( message: "Please enter a valid amount")
+            alert(message: "Please enter your bill amount")
             return
         }
         
-        guard let billAmount = Float(amount) else {
-            alert(message: "Please enter proper age.")
-            return
-        }
+        // convert the bill amount into double
+        billAmount = Double(amount)!
+        
+        calculateTipsAndBillAmout()
+        
+    }
+    
+    func calculateTipsAndBillAmout(){
+        tipAmount = billAmount + (billAmount * Double(tipPercent) / 100.0)
+        billAmountPerPerson = tipAmount / Double(partySize)
     }
     
     @IBAction func tipPercentageStepper(_ sender: UIStepper) {
+        tipPercent = Int(sender.value)
+        calculateTipsAndBillAmout()
     }
     
     @IBAction func partySizeSlidder(_ sender: UISlider) {
+        partySize = Int(sender.value)
+        calculateTipsAndBillAmout()
     }
+    
+    func updateView(){
+        tipPercentLabel.text = String(tipPercent) + "%"
+        partySizeLabel.text = String(partySize)
+        
+        tipAmountLabel.text = "$" + String(tipAmount)
+        billAmountPerPersonLabel.text = "$" + String(billAmountPerPerson)
+    }
+    
     /**
     Displays alert in the app with a custome message and a close button to close alert.
      
@@ -60,6 +110,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
         alertController.addAction(cancelAction)
         self.present(alertController, animated: true, completion: nil)
     }
-
+    
+    /**
+    * Closes the keyboard when user touches on other parts of the screen
+    * and also converts the amountTextField to floating point and checks if the field is valid or not using
+    *  amoutFieldDelegate class
+    */
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        self.view.endEditing(true)
+//    }
 }
 
