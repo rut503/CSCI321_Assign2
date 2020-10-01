dd4124384dcf4e0ee9a4b3e501ce7c713b4e4622
rut503 <rutvik503@gmail.com>
2020-10-01 17:06:12 -0500

    documented and currency formatted

--- CSCI321_Assign2/ViewController.swift
+++ CSCI321_Assign2/ViewController.swift
@@ -2,7 +2,8 @@
 //  ViewController.swift
 //  CSCI321_Assign2
 //
-//  Created by Rutvik Patel on 9/28/20.
+//  Created by Rutvik Patel (Z1865128).
+//  Created by Aviraj Parmar (Z1861160).
 //  Copyright © 2020 Rut Codes. All rights reserved.
 //
 
@@ -12,7 +13,6 @@ class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var amountTextField: UITextField!
     
-    
     @IBOutlet weak var tipPercentLabel: UILabel!
     @IBOutlet weak var tipAmountLabel: UILabel!
     
@@ -20,31 +20,31 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var billAmountPerPersonLabel: UILabel!
     
     var billAmount: Double = 0.0 {
-        didSet{
+        didSet{ // updating view if the value of billAmount changes
             updateView()
         }
     }
     
     var tipPercent: Int = 20 {
-        didSet{
+        didSet{ // updating view if the value of tipPercent changes
             updateView()
         }
     }
     
     var tipAmount: Double = 0.0 {
-        didSet{
+        didSet{ // updating view if the value of tipAmount changes
             updateView()
         }
     }
     
     var partySize: Int = 1 {
-        didSet{
+        didSet{ // updating view if the value of partySize changes
             updateView()
         }
     }
     
     var billAmountPerPerson: Double = 0.0 {
-        didSet{
+        didSet{ // updating view if the value of billAmountPerPerson changes
             updateView()
         }
     }
@@ -54,10 +54,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // assigning delegate to amount text field
         amountTextField.delegate = delegate
     }
     
+    /**
+     Validates user bill amount input and calls function to calculate tip amount and bill amount per person.
+     */
     @IBAction func enterBillAmountButtonPress(_ sender: UIButton) {
         
         // hide the keyboard
@@ -73,34 +77,61 @@ class ViewController: UIViewController, UITextFieldDelegate {
         billAmount = Double(amount)!
         
         calculateTipsAndBillAmout()
-        
     }
     
+    /**
+     Calculates and sets the proper variables to the amount of tip and bill amount per person according to user input.
+     */
     func calculateTipsAndBillAmout(){
         tipAmount = billAmount + (billAmount * Double(tipPercent) / 100.0)
         billAmountPerPerson = tipAmount / Double(partySize)
     }
     
+    /**
+     Sets the value of the tip percentage variable according to the stepper and calls function to calculate tip amount
+     and bill amount per person.
+     */
     @IBAction func tipPercentageStepper(_ sender: UIStepper) {
         tipPercent = Int(sender.value)
         calculateTipsAndBillAmout()
     }
     
+    /**
+     Sets the value of the party size variable according to the slider and calls function to calculate tip amount
+     and bill amount per person.
+     */
     @IBAction func partySizeSlidder(_ sender: UISlider) {
         partySize = Int(sender.value)
         calculateTipsAndBillAmout()
     }
     
+    /**
+     Updates view by updating all the amount labels on the screen according to the user input.
+     */
     func updateView(){
         tipPercentLabel.text = String(tipPercent) + "%"
         partySizeLabel.text = String(partySize)
         
-        tipAmountLabel.text = "$" + String(tipAmount)
-        billAmountPerPersonLabel.text = "$" + String(billAmountPerPerson)
+        tipAmountLabel.text = convertDoubleToCurrency(num: tipAmount)
+        billAmountPerPersonLabel.text = convertDoubleToCurrency(num: billAmountPerPerson)
     }
     
     /**
-    Displays alert in the app with a custome message and a close button to close alert.
+     Converts a number of type double to a type string and formats it to look like a currency.
+     
+     - Parameter num: Number being converted and formatted
+     
+     - Returns: A string of currency amount
+     */
+    func convertDoubleToCurrency(num: Double) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        
+        return numberFormatter.string(from: NSNumber(value: num))!
+    }
+    
+    /**
+     Displays alert in the app with a custome message and a close button to close alert.
      
      - Parameter message: The custom alert message.
     */
@@ -110,14 +141,5 @@ class ViewController: UIViewController, UITextFieldDelegate {
         alertController.addAction(cancelAction)
         self.present(alertController, animated: true, completion: nil)
     }
-    
-    /**
-    * Closes the keyboard when user touches on other parts of the screen
-    * and also converts the amountTextField to floating point and checks if the field is valid or not using
-    *  amoutFieldDelegate class
-    */
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        self.view.endEditing(true)
-//    }
 }
 
